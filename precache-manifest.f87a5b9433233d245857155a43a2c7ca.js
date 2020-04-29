cc9587ba15804e83922dccd73ff27abe1c2a5cd6
Jyotirmoy Ghosh <jrmoyghosh@gmail.com>
2020-04-29 15:44:11 -0500

    Updates

---
+++ precache-manifest.f87a5b9433233d245857155a43a2c7ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2936e7f7213903cad48e85f36c00b7",
+    "url": "/businessteam/index.html"
+  },
+  {
+    "revision": "21be265d3f5cd15c7704",
+    "url": "/businessteam/static/css/main.371fc773.chunk.css"
+  },
+  {
+    "revision": "8fd2c080061240670a57",
+    "url": "/businessteam/static/js/2.3c686b32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/businessteam/static/js/2.3c686b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21be265d3f5cd15c7704",
+    "url": "/businessteam/static/js/main.2d03cc19.chunk.js"
+  },
+  {
+    "revision": "04f6d2af9ef861687795",
+    "url": "/businessteam/static/js/runtime-main.ebab640e.js"
+  }
+]);
